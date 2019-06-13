a0acaf1d3a50c64d8efd7d6da530b79aa6e77bd0
syota-shidara <ss045.official@gmail.com>
2019-06-13 09:42:29 +0000

    echiechi

--- app/models/disc.rb
+++ app/models/disc.rb
@@ -1,5 +1,5 @@
 class Disc < ApplicationRecord
 	  belongs_to :product
-	  has_many :songs, inverse_of: :disc
+	  has_many :songs
 	  accepts_nested_attributes_for :songs, reject_if: :all_blank, allow_destroy: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
-	has_many :discs, inverse_of: :product
+	has_many :discs
 	accepts_nested_attributes_for :discs, reject_if: :all_blank, allow_destroy: true
 end
