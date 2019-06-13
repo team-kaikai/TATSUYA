7f45ac996dc63062b71eeedd50fdb0d3e0d4a102
oosawagithub <newstation1215@gmail.com>
2019-06-13 16:24:39 +0900

    root変更

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,4 +1,13 @@
 class Admins::GenresController < ApplicationController
   def create
+  	@genre = Genre.new(genre_params)
+  	@genre.save
   end
+  def new
+  end
+
+  private
+  	def genre_params
+  		params.require(:genre).permit(:name)
+  	end
 end
--- app/controllers/admins/labels_controller.rb
+++ app/controllers/admins/labels_controller.rb
@@ -1,4 +1,13 @@
 class Admins::LabelsController < ApplicationController
   def create
+  	@label = Label.new(label_params)
+  	@label.save
   end
+  def new
+  end
+
+  private
+  	def label_params
+  		params.require(:label).permit(:name)
+  	end
 end
