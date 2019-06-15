8d49572875a13a4f1a28af1ccf0d2150c4f60a11
syota-shidara <ss045.official@gmail.com>
2019-06-15 06:42:33 +0000

    update7

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,10 +3,12 @@ class ProductsController < ApplicationController
   end
 
   def show
-  	#@product = Product.find(params[:id])
+  	@product = Product.find(params[:id])
     #@genre = product.genre
     #@label = product.label
     #@artist = product.artis
+    #@disc = product.disc.songs
+    #@products = Product.all.order(created_at: :desc)
   end
 
 end
