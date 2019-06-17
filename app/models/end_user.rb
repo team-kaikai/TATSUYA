78dab1bfc84dae6f855c44a9aca101087e48263f
syota-shidara <ss045.official@gmail.com>
2019-06-17 06:21:13 +0000

    updare8

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -4,7 +4,7 @@ class EndUser < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :cars, dependent: :destroy
+  has_many :carts, dependent: :destroy
   
 
 
