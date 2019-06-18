9dea21c7fe365c9edb1c6b7912f3748a4e468426
oosawagithub <newstation1215@gmail.com>
2019-06-18 16:45:10 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_074310) do
+ActiveRecord::Schema.define(version: 2019_06_17_031852) do
 
   create_table "address_menus", force: :cascade do |t|
     t.text "address"
