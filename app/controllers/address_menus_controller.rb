971fb97d2f2bdf5029b91a3e3b2727d14a7ce4ef
riekatahira <fuyu.g.0912@gmail.com>
2019-06-19 10:18:30 +0000

    commit

--- app/controllers/address_menus_controller.rb
+++ app/controllers/address_menus_controller.rb
@@ -2,7 +2,7 @@ class AddressMenusController < ApplicationController
   def create
   	 @address_menus = AddressMenu.new(address_menu_params)
   	 @address_menus.enduser_id = current_end_user.id
-  	 @address_menus.save!
+  	 @address_menus.save
   	 redirect_to new_end_user_address_menus_path(current_end_user.id)
   end
 
