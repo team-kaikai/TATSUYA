f051ed7290019a9c3b8ba2587022d9b364eaf22a
oosawagithub <newstation1215@gmail.com>
2019-06-19 16:29:05 +0900

    6/19

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -31,6 +31,8 @@ class Admins::ProductsController < ApplicationController
     @search = Product.ransack(params[:q])
     # 検索結果
     @products = @search.result
+    #ランキング実装用
+    @all_ranks = Product.find(Favorite.group(:product_id).order('count(product_id)desc').limit(5).pluck(:product_id))
   end
 
   def edit
