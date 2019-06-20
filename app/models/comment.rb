cd556ab05ac050fa7f2db45cf76174603ce69db8
fieldwanko <field.kou815@gmail.com>
2019-06-20 06:26:26 +0000

    [update]

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
 	belongs_to :end_user, class_name: "EndUser", foreign_key: "enduser_id"
 	belongs_to :product
+
+	validates :comment,length:{ in:1..200 }
 end
--- app/models/order_append.rb
+++ app/models/order_append.rb
@@ -2,7 +2,11 @@ class OrderAppend < ApplicationRecord
 
 	has_many :order_details
 	belongs_to :end_user, class_name: "EndUser", foreign_key: "enduser_id"
-	
+
+
+	validates :name,presence: true
+    validates :address,length:{ in: 10..20}
+
 # enum記述
 	enum status: { 未配送: 0, 配送中: 1, 配送完了: 2}
 
