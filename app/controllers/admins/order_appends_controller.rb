17c88b5b2c9bb4d33288537e50c23499265bd37d
oosawagithub <newstation1215@gmail.com>
2019-06-22 16:28:43 +0900

    update

--- app/controllers/admins/order_appends_controller.rb
+++ app/controllers/admins/order_appends_controller.rb
@@ -1,9 +1,21 @@
 class Admins::OrderAppendsController < ApplicationController
   def show
+  	@order_append = OrderAppend.find(params[:id])
+  	@order_details = OrderDetail.all
   end
 
   def index
   	@order_appends = OrderAppend.all
-
   end
+
+  def update
+  	@order_append = OrderAppend.find(params[:id])
+  	@order_append.update(order_append_params)
+  	redirect_to admins_order_append_path(@order_append.id)
+  end
+
+  private
+  	def order_append_params
+  		params.require(:order_append).permit(:status)
+  	end
 end
--- app/controllers/order_appends_controller.rb
+++ app/controllers/order_appends_controller.rb
@@ -36,14 +36,12 @@ class OrderAppendsController < ApplicationController
         @order_detail.product_id = cart.product.id
         @order_detail.price = cart.product.price
         @order_detail.quantity = cart.quantity
-        binding.pry
         @order_detail.save!
       end
 
 
-
-
-    @order_append.update(order_append_params)
+    @order_append.pay =  params[:order_append][:pay].to_i
+    @order_append.save
     redirect_to home_success_path
   end
 
