86d7ccae698c891a236ec8824d1e822031d9b4e6
oosawagithub <newstation1215@gmail.com>
2019-06-23 15:41:41 +0900

    6/23 15:41

--- config/routes.rb
+++ config/routes.rb
@@ -121,7 +121,7 @@ end
 
 	 resources :labels, only: [:create,:new]
 
-   resources :order_appends,only:[:show,:index]
+   resources :order_appends,only:[:show,:index,:update]
  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
