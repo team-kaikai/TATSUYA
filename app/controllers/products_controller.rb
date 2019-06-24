427dde5e69e37860e9e35ffe89ba1b1418dfe1f4
oosawagithub <newstation1215@gmail.com>
2019-06-24 18:32:51 +0900

    commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,11 @@
 class ProductsController < ApplicationController
   def index
-  	@products = Product.all.order(created_at: :desc)
+  	@products = Product.all.order(created_at: :desc).page(params[:page]).per(9)
     # 検索オブジェクト
-    @search = Product.ransack(params[:q])
+    if @search = Product.ransack(params[:q])
     # 検索結果
     @products = @search.result
+    end
   	#ランキング実装用
   	@all_ranks = Product.find(Favorite.group(:product_id).order('count(product_id)desc').limit(5).pluck(:product_id))
     #スクロール
